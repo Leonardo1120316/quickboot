7e9cb3fb0e8c5c8f4cba53f664e00b9d251c2e5f
Leonardo1120316 <w18968850835@163.com>
2025-03-08 17:39:55 +0800

    后端分模块

---
+++ backend/quickboot-common/src/main/java/com/quickboot/QuickbootCommonApplication.java
@@ -0,0 +1,13 @@
+package com.quickboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbootCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickbootCommonApplication.class, args);
+    }
+
+}
